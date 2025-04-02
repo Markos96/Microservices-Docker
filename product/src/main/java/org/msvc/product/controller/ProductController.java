c028697ab050fa3c2ee6c2ccc48355509d64ad71
Markos96 <markossolari@gmail.com>
2025-04-02 17:18:40 -0300

    Create Controller and Service Logic

---
+++ product/src/main/java/org/msvc/product/controller/ProductController.java
@@ -0,0 +1,24 @@
+package org.msvc.product.controller;
+
+import org.msvc.product.data.domain.Product;
+import org.msvc.product.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return this.productService.getAllProducts();
+    }
+}
---
+++ product/src/main/java/org/msvc/product/service/ProductService.java
@@ -0,0 +1,19 @@
+package org.msvc.product.service;
+
+import org.msvc.product.data.domain.Product;
+import org.msvc.product.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
